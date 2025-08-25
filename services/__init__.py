7411d4e8057ed280d77a88e2553ef3d76c582f50
Roman Logov <roman@bs1.ru>
2025-08-26 06:15:42 +0800

    fix yandexgpt

--- services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Services module for Okama Finance Bot
-
-This module provides services for financial analysis using the Okama library.
-"""
-
-from .asset_service import AssetService
-
-__all__ = ['AssetService']
